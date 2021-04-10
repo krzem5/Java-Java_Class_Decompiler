338517298d75aa16d791fec60ce680d39e1219e9
Krzem <krzem5.dev@gmail.com>
2021-04-10 09:34:58 +0200

    Push Update 04/10/2021, 09:34:32

--- build.bat
+++ build.bat
@@ -1,22 +1,3 @@
 @echo off
 cls
-if exist build rmdir /s /q build
-mkdir build
-cd src
-javac -d ../build com/krzem/java_class_decompiler/Main.java&&jar cvmf ../manifest.mf ../build/java_class_decompiler.jar -C ../build *&&goto run
-cd ..
-goto end
-:run
-cd ..
-pushd "build"
-for /D %%D in ("*") do (
-	rd /S /Q "%%~D"
-)
-for %%F in ("*") do (
-	if /I not "%%~nxF"=="java_class_decompiler.jar" del "%%~F"
-)
-popd
-javac test.java
-cls
-java -jar build/java_class_decompiler.jar
-:end
+python build.py %*
